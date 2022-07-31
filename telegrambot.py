d2436499c2c524ba846eeb596d6a195f5bb2bfd0
Mikhail Larin <larin.mikhail.84@mai.ru>
2022-07-31 13:36:28 +0700

    2nd commit

--- telegrambot.py
+++ telegrambot.py
@@ -23,7 +23,14 @@ def get_datedoge():
     response = req.json()
     print(response)
     sell_price = response['doge_usd']['sell']
-    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Dogecoin price: {sell_price}')     
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Dogecoin price: {sell_price}')   
+
+def get_dateusdt():
+    req = requests.get("https://yobit.net/api/3/ticker/usdt_usd")
+    response = req.json()
+    print(response)
+    sell_price = response['usdt_usd']['sell']
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell USDT price: {sell_price}')   
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
@@ -33,6 +40,7 @@ def telegram_bot(token):
         bot.send_message(message.chat.id, 'Write "ethereum" in order to find out the current price of Ethereum')
         bot.send_message(message.chat.id, 'Write "bitcoin" in order to find out the current price of Bitcoin')
         bot.send_message(message.chat.id, 'Write "dogecoin" in order to find out the current price of Dogecoin')
+        bot.send_message(message.chat.id, 'Write "usdt" in order to find out the current price of USDT')
 
     @bot.message_handler(content_types = ["text"])
     def send_text(message):
@@ -65,12 +73,23 @@ def telegram_bot(token):
             except Exception as ex:
                 print(ex)
                 bot.send_message(message.chat.id, "Something was wrong...")
+
+        elif message.text.lower() == "usdt":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/usdt_usd")
+                response = req.json()
+                sell_price = response['usdt_usd']['sell']
+                bot.send_message(message.chat.id, f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell USDT price: {sell_price}')
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Something was wrong...")
         else:
-            bot.send_message(message.chat.id, "I am sorry but I am not understand you(")
+            bot.send_message(message.chat.id, "I don't quite understand you")
 
     bot.polling()
 if __name__ == '__main__':
     #get_date()
-    #get_datebtc
-    #get_datedoge
+    #get_datebtc()
+    #get_datedoge()
+    #get_dateusdt()
     telegram_bot(token)
