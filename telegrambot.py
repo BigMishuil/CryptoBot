bc079a950f5714406b2d0da537c585fa6fa21681
Mikhail Larin <larin.mikhail.84@mai.ru>
2022-07-31 12:54:08 +0700

    1st commit

---
+++ telegrambot.py
@@ -0,0 +1,76 @@
+import requests
+from datetime import datetime
+import telebot
+from auth_data import token
+
+bot = telebot.TeleBot(token)
+def get_date():
+    req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+    response = req.json()
+    print(response)
+    sell_price = response['eth_usd']['sell']
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Ethereum price: {sell_price}')
+
+def get_datebtc():
+    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+    response = req.json()
+    print(response)
+    sell_price = response['btc_usd']['sell']
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Bitcoin price: {sell_price}')
+
+def get_datedoge():
+    req = requests.get("https://yobit.net/api/3/ticker/doge_usd")
+    response = req.json()
+    print(response)
+    sell_price = response['doge_usd']['sell']
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Dogecoin price: {sell_price}')     
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello!")
+        bot.send_message(message.chat.id, 'Write "ethereum" in order to find out the current price of Ethereum')
+        bot.send_message(message.chat.id, 'Write "bitcoin" in order to find out the current price of Bitcoin')
+        bot.send_message(message.chat.id, 'Write "dogecoin" in order to find out the current price of Dogecoin')
+
+    @bot.message_handler(content_types = ["text"])
+    def send_text(message):
+        if message.text.lower() == "ethereum":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+                response = req.json()
+                sell_price = response['eth_usd']['sell']
+                bot.send_message(message.chat.id, f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Ethereum price: {sell_price}')
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Something was wrong...")
+
+        elif message.text.lower() == "bitcoin":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response['btc_usd']['sell']
+                bot.send_message(message.chat.id, f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Bitcoin price: {sell_price}')
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Something was wrong...")
+
+        elif message.text.lower() == "dogecoin":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/doge_usd")
+                response = req.json()
+                sell_price = response['doge_usd']['sell']
+                bot.send_message(message.chat.id, f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell Dogecoin price: {sell_price}')
+            except Exception as ex:
+                print(ex)
+                bot.send_message(message.chat.id, "Something was wrong...")
+        else:
+            bot.send_message(message.chat.id, "I am sorry but I am not understand you(")
+
+    bot.polling()
+if __name__ == '__main__':
+    #get_date()
+    #get_datebtc
+    #get_datedoge
+    telegram_bot(token)
